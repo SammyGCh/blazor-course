7f0fc98f846633b6f661ee089763042915657882
Sammy <sammygch.1@gmail.com>
2022-09-11 18:36:22 -0500

    creacion del proyecto FirstBlazorApp

---
+++ FirstBlazorApp/Data/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazorApp.Data
+{
+    public class TodoItem
+    {
+        public string Todo { get; set; } = string.Empty;
+    }
+}
---
+++ FirstBlazorApp/ForecastController.cs
@@ -0,0 +1,23 @@
+﻿using FirstBlazorApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazorApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForecastController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> Get()
+        {
+            var svc = new WeatherForecastService();
+
+            return new OkObjectResult(await svc.GetForecastAsync(DateTime.Now));
+        }
+    }
+}
