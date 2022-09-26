5a0fbe848374ef5c850e3fe18a6f1e89367cdce4
Sammy <sammygch.1@gmail.com>
2022-09-26 13:42:54 -0500

    creación de proyecto de blazor server

---
+++ BlazorAppServer/Utilerias/ClienteHttp.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorAppServer.Utilerias
+{
+    public class ClienteHttp : IClienteHttp
+    {
+        private readonly HttpClient _cliente;
+        private HttpResponseMessage _httpResponseMessage;
+
+        public ClienteHttp(HttpClient cliente)
+        {
+            _cliente = cliente;
+        }
+
+        public async Task<Response> PeticionGetAsync<Response>(string url)
+        {
+            Response respuesta;
+            try
+            {
+                _httpResponseMessage = await _cliente.GetAsync(url);
+
+                if (!_httpResponseMessage.IsSuccessStatusCode)
+                {
+                    throw new Exception("No se pudo conectar al servicio.");
+                }
+
+                string respuestaJson = await _httpResponseMessage.Content.ReadAsStringAsync();
+
+                respuesta = JsonConvert.DeserializeObject<Response>(respuestaJson);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return respuesta;
+        }
+    }
+}
---
+++ BlazorAppServer/Utilerias/IClienteHttp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppServer.Utilerias
+{
+    public interface IClienteHttp
+    {
+        Task<Response> PeticionGetAsync<Response>(string url);
+    }
+}
